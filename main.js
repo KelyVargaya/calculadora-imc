0091fdddfb147ac9e6eaef92815dbddd74eb72be
Giovanna Oruro <orurog@gmail.com>
2017-07-17 14:40:17 -0500

    Cambios Finales

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-function persona (){
-    var masa = "";
-  var persona = prompt("pon tu masa corporal");
-    
-   if (persona == masa){
-       
-   }
+var peso = prompt("Ingrese su peso","Kg");
+var altura = prompt("Ingrese su altura","Mts");
+var imc;
+
+    imc=peso/(altura*altura);
+
+    if (imc > 18 && imc< 24.9){
+     alert("Su Indice de Masa Corporal es: "+ imc.toFixed(2) + " Y esta en un rango NORMAL");
+
+    }
+
+    else if (imc > 25 && imc <26.9){
+    alert("Su Indice de Masa Corporal es: "+ imc.toFixed(2) + " Y esta en un rango de SOBREPESO");
 }
-console.log(persona);
+
+	else if (imc>27){
+		 alert("Su Indice de Masa Corporal es: "+ imc.toFixed(2) + " Y esta en un rango de OBESIDAD");
+	}
