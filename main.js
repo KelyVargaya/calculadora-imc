c54c2fb55626b8209cab16db2560b2f3dd04dffb
luisaGonzales <luisagc711@gmail.com>
2017-07-19 09:56:58 -0500

    Modificaciones de HTML y JS con input

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-var peso = prompt("Ingrese su peso","Kg");
-var altura = prompt("Ingrese su altura","Mts");
-var imc;
+function calcularIMC (peso, altura){
+  var peso = parseInt(document.getElementById("kilos").value);
+  var altura = parseFloat(document.getElementById("altura").value);
+  var resultado = document.getElementById("resultado");
+  var imc;
+  var res = "";
 
-    imc=peso/(altura*altura);
+    imc = peso/(Math.pow(altura,2));
 
-    if (imc > 18 && imc< 24.9){
-     alert("Su Indice de Masa Corporal es: "+ imc.toFixed(2) + " Y esta en un rango NORMAL");
+    if (imc > 18 && imc < 24.9){
+      res += "Su Indice de Masa Corporal es: "+ imc.toFixed(2) + " \nY esta en un rango NORMAL";
 
     }
 
-    else if (imc > 25 && imc <26.9){
-    alert("Su Indice de Masa Corporal es: "+ imc.toFixed(2) + " Y esta en un rango de SOBREPESO");
-}
+    else if (imc > 25 && imc < 26.9){
+      res += "Su Indice de Masa Corporal es: "+ imc.toFixed(2) + " \nY esta en un rango de SOBREPESO";
+    }
 
-	else if (imc>27){
-		 alert("Su Indice de Masa Corporal es: "+ imc.toFixed(2) + " Y esta en un rango de OBESIDAD");
-	}
+	  else if (imc > 27){
+      res += "Su Indice de Masa Corporal es: "+ imc.toFixed(2) + " \nY esta en un rango de OBESIDAD";
+	  }
+
+  return alert(res);
+}
